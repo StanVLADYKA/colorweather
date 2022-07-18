9e9d2bae8e93754920e734ef7d21d7e73bc67d71
StanVladyka <stasglow1@gmail.com>
2022-07-19 01:38:27 +0300

    1

--- CW/cw6/admin.py
+++ CW/cw6/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Days
+
+admin.site.register(Days)
 
 # Register your models here.
--- CW/cw6/models.py
+++ CW/cw6/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Days (models.Model):
+    #date = models.DateField(blank=False)
+    date = models.CharField(max_length=10,help_text='first day',blank=True)
+    d1 = models.CharField(max_length=4)
+    d2 = models.IntegerField()
+    d3 = models.IntegerField
+    d4 = models.IntegerField
+    d5 = models.IntegerField
+    d6 = models.IntegerField
+    d7 = models.IntegerField
+
+    def __str__(self):
+        return self.date
+
 # Create your models here.
