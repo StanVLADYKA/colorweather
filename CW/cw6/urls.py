430fe57be266f8d9984ba5f86c5629d0b0661168
StanVladyka <stasglow1@gmail.com>
2022-07-27 20:32:29 +0300

    baton

--- CW/cw6/urls.py
+++ CW/cw6/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('test/', views.test, name = 'test')
-    #path('', views.index, name = 'index'),
-    #path('', views.index, name = 'index'),
     path('', views.give_temp, name = 'give_temp'),
 ]
